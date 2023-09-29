f4ed69dddad0961a87ce8cb5b9b21b191b867b0d
Ian Teda <ian@teda.id.au>
2023-09-29 21:07:28 +1000

    Install mini animate

---
+++ lua/plugins/mini_animate.lua
@@ -0,0 +1,130 @@
+-------------------------------------------------------------------------------
+-- MINI ANIMATE
+-- Animate common Neovim actions
+--
+-- File: ~/.config/nvim/lua/plugins/mini_animate.lua
+-- Repo: https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-animate.md 
+-------------------------------------------------------------------------------
+
+return {
+    -- PLUGIN TO LAZY LOAD
+    -- Lazy.vim will look for lua files in the ~/.config/nvim/lua/plugins folder
+    'echasnovski/mini.animate', 
+    
+    -- VERSIONING
+    -- If you want to install a specific revision of a plugin, you can use 
+    -- `commit`,`tag`, `branch`, `version`.
+    version = "*",
+    
+    -- KEY MAPPING
+    -- Key mappings will load the plugin the first time they get executed.
+    -- keys = {"<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree"} ,
+
+    -- DEPENDENCIES
+    -- A list of plugin names or plugin specs that should be loaded when the 
+    -- plugin loads. Dependencies are always lazy-loaded unless specified 
+    -- otherwise. When specifying a name, make sure the plugin spec has been
+    -- defined somewhere else.
+    -- dependencies = { 'github_username/repo_name'},
+
+    -- LAZY
+    -- When true, the plugin will only be loaded when needed. Lazy-loaded 
+    -- plugins are automatically loaded when their Lua modules are required, 
+    -- or when one of the lazy-loading handlers triggers
+    -- lazy = true,
+
+    -- COMMAND
+    -- Lazy load on command 
+    -- cmd = {},
+
+    -- FILETYPE
+    -- Lazy load on filetype
+    -- ft = {},
+
+    -- PRIORITY
+    -- Only useful for start plugins (lazy=false) to force loading certain 
+    -- plugins first. Default priority is 50. It’s recommended to set this to 
+    -- a high number for colorschemes.
+    -- priority = number?,
+
+    -- OPTIONAL
+    -- When a spec is tagged optional, it will only be included in the 
+    -- final spec, when the same plugin has been specified at least once 
+    -- somewhere else without optional. This is mainly useful for Neovim 
+    -- distros, to allow setting options on plugins that may/may not be part 
+    -- of the user’s plugins
+    -- optional = boolean?,
+
+    -- SETTINGS
+    -- opts should be a table (will be merged with parent specs), return a 
+    -- table (replaces parent specs) or should change a table. The table will 
+    -- be passed to the Plugin.config() function. Setting this value will imply
+    -- Plugin.config()
+    opts = {
+        -- Cursor path
+        cursor = {
+            -- Whether to enable this animation
+            enable = true,
+
+            -- Timing of animation (how steps will progress in time)
+            -- timing = --<function: implements linear total 250ms animation duration>,
+
+            -- Path generator for visualized cursor movement
+            -- path = --<function: implements shortest line path>,
+        },
+
+        -- Vertical scroll
+        scroll = {
+            -- Whether to enable this animation
+            enable = true,
+
+            -- Timing of animation (how steps will progress in time)
+            -- timing = --<function: implements linear total 250ms animation duration>,
+
+            -- Subscroll generator based on total scroll
+            -- subscroll = --<function: implements equal scroll with at most 60 steps>,
+        },
+
+        -- Window resize
+        resize = {
+            -- Whether to enable this animation
+            enable = true,
+
+            -- Timing of animation (how steps will progress in time)
+            -- timing = --<function: implements linear total 250ms animation duration>,
+
+            -- Subresize generator for all steps of resize animations
+            -- subresize = --<function: implements equal linear steps>,
+        },
+
+        -- Window open
+        open = {
+            -- Whether to enable this animation
+            enable = true,
+
+            -- Timing of animation (how steps will progress in time)
+            -- timing = --<function: implements linear total 250ms animation duration>,
+
+            -- Floating window config generator visualizing specific window
+            -- winconfig = --<function: implements static window for 25 steps>,
+
+            -- 'winblend' (window transparency) generator for floating window
+            -- winblend = --<function: implements equal linear steps from 80 to 100>,
+        },
+
+        -- Window close
+        close = {
+            -- Whether to enable this animation
+            enable = true,
+
+            -- Timing of animation (how steps will progress in time)
+            -- timing = --<function: implements linear total 250ms animation duration>,
+
+            -- Floating window config generator visualizing specific window
+            -- winconfig = --<function: implements static window for 25 steps>,
+
+            -- 'winblend' (window transparency) generator for floating window
+            -- winblend = --<function: implements equal linear steps from 80 to 100>,
+        },
+    }
+}
--- lua/plugins/mini_indentscope.lua
+++ lua/plugins/mini_indentscope.lua
@@ -1,52 +1,51 @@
 -------------------------------------------------------------------------------
--- NAME OF PLUGIN
--- Description of plugin 
+-- MINI INDENT SCOPE
+-- Animate common Neovim actions
 --
--- File: /location/of/file 
--- Repo: http://github.com/username/repo 
+-- File: ~/.config/nvim/lua/plugins/mini_indentscope.lua
+-- Repo:https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-indentscope.md 
 -------------------------------------------------------------------------------
 
 return {
     -- PLUGIN TO LAZY LOAD
     -- Lazy.vim will look for lua files in the ~/.config/nvim/lua/plugins folder
-    --'github_username/repo_name',
+   'echasnovski/mini.indentscope', 
     
     -- VERSIONING
     -- If you want to install a specific revision of a plugin, you can use 
     -- `commit`,`tag`, `branch`, `version`.
-    --version = "*",
+   version = "*",
     
     -- KEY MAPPING
     -- Key mappings will load the plugin the first time they get executed.
     -- keys = {"<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree"} ,
-    --keys = {},
 
     -- DEPENDENCIES
     -- A list of plugin names or plugin specs that should be loaded when the 
     -- plugin loads. Dependencies are always lazy-loaded unless specified 
     -- otherwise. When specifying a name, make sure the plugin spec has been
     -- defined somewhere else.
-    --dependencies = { 'github_username/repo_name'},
+    -- dependencies = { 'github_username/repo_name'},
 
     -- LAZY
     -- When true, the plugin will only be loaded when needed. Lazy-loaded 
     -- plugins are automatically loaded when their Lua modules are required, 
     -- or when one of the lazy-loading handlers triggers
-    --lazy = true,
+    -- lazy = true,
 
     -- COMMAND
     -- Lazy load on command 
-    --cmd = {},
+    -- cmd = {},
 
     -- FILETYPE
     -- Lazy load on filetype
-    --ft = {},
+    -- ft = {},
 
     -- PRIORITY
     -- Only useful for start plugins (lazy=false) to force loading certain 
     -- plugins first. Default priority is 50. It’s recommended to set this to 
     -- a high number for colorschemes.
-    --priority = number?,
+    -- priority = number?,
 
     -- OPTIONAL
     -- When a spec is tagged optional, it will only be included in the 
@@ -54,14 +53,59 @@ return {
     -- somewhere else without optional. This is mainly useful for Neovim 
     -- distros, to allow setting options on plugins that may/may not be part 
     -- of the user’s plugins
-    --optional = boolean?,
+    -- optional = boolean?,
 
     -- SETTINGS
     -- opts should be a table (will be merged with parent specs), return a 
     -- table (replaces parent specs) or should change a table. The table will 
     -- be passed to the Plugin.config() function. Setting this value will imply
     -- Plugin.config()
-    --opts = {}
+    opts = {
+        -- Draw options
+        draw = {
+            -- Delay (in ms) between event and start of drawing scope indicator
+            delay = 100,
+
+            -- Animation rule for scope's first drawing. A function which, given
+            -- next and total step numbers, returns wait time (in ms). See
+            -- |MiniIndentscope.gen_animation| for builtin options. To disable
+            -- animation, use `require('mini.indentscope').gen_animation.none()`.
+            -- animation = --<function: implements constant 20ms between steps>,
+
+            -- Symbol priority. Increase to display on top of more symbols.
+            priority = 2,
+        },
+
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+            -- Textobjects
+            object_scope = 'ii',
+            object_scope_with_border = 'ai',
+
+            -- Motions (jump to respective border line; if not present - body line)
+            goto_top = '[i',
+            goto_bottom = ']i',
+        },
+
+        -- Options which control scope computation
+        options = {
+            -- Type of scope's border: which line(s) with smaller indent to
+            -- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
+            border = 'both',
+
+            -- Whether to use cursor column when computing reference indent.
+            -- Useful to see incremental scopes with horizontal cursor movements.
+            indent_at_cursor = true,
+
+            -- Whether to first check input line to be a border of adjacent scope.
+            -- Use it if you want to place cursor on function header to get scope of
+            -- its body.
+            try_as_border = false,
+        },
+
+        -- Which character to use for drawing scope indicator
+        symbol = '╎',
+    },
 
     -- CONFIG 
     -- Config is executed when the plugin loads. The default implementation 
@@ -69,9 +113,9 @@ return {
     -- heuristics to determine the plugin’s MAIN module automatically based 
     -- on the plugin’s name. See also opts. To use the default implementation 
     -- without opts set config to true.
-    --config = function ()
-    --    -- Put plugin settings into a local variable for easier reading
-    --    local PLUGIN_SETTINGS = {
+    -- config = function ()
+    --     ---@class PLUGIN_SETTINGS
+    --     local PLUGIN_SETTINGS = {
     --    }
     --    require('plugin').setup(PLUGIN_SETTINGS)
     --end,
