385c5a70840b21a2ecea79b97d5f09547bee3844
Ian Teda <ian@teda.id.au>
2023-09-30 14:29:53 +1000

    Setup trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,136 @@
+-------------------------------------------------------------------------------
+-- TROUBLE
+-- A pretty list for showing diagnostics, references, telescope results,
+-- quickfix and location lists to help you solve all the trouble your code is
+-- causing.
+--
+-- File: ~/.config/nvim/lua/plugins/trouble.lua
+-- Repo: https://github.com/folke/trouble.nvim
+-------------------------------------------------------------------------------
+
+return {
+	-- PLUGIN TO LAZY LOAD
+	-- Lazy.vim will look for lua files in the ~/.config/nvim/lua/plugins folder
+	"folke/trouble.nvim",
+
+	-- VERSIONING
+	-- If you want to install a specific revision of a plugin, you can use
+	-- `commit`,`tag`, `branch`, `version`.
+	version = "*",
+
+	-- KEY MAPPING
+	-- Key mappings will load the plugin the first time they get executed.
+	-- keys = {"<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree"} ,
+	--keys = {},
+
+	-- DEPENDENCIES
+	-- A list of plugin names or plugin specs that should be loaded when the
+	-- plugin loads. Dependencies are always lazy-loaded unless specified
+	-- otherwise. When specifying a name, make sure the plugin spec has been
+	-- defined somewhere else.
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	-- LAZY
+	-- When true, the plugin will only be loaded when needed. Lazy-loaded
+	-- plugins are automatically loaded when their Lua modules are required,
+	-- or when one of the lazy-loading handlers triggers
+	--lazy = true,
+
+	-- COMMAND
+	-- Lazy load on command
+	--cmd = {},
+
+	-- FILETYPE
+	-- Lazy load on filetype
+	--ft = {},
+
+	-- PRIORITY
+	-- Only useful for start plugins (lazy=false) to force loading certain
+	-- plugins first. Default priority is 50. It’s recommended to set this to
+	-- a high number for colorschemes.
+	--priority = number?,
+
+	-- OPTIONAL
+	-- When a spec is tagged optional, it will only be included in the
+	-- final spec, when the same plugin has been specified at least once
+	-- somewhere else without optional. This is mainly useful for Neovim
+	-- distros, to allow setting options on plugins that may/may not be part
+	-- of the user’s plugins
+	--optional = boolean?,
+
+	-- SETTINGS
+	-- opts should be a table (will be merged with parent specs), return a
+	-- table (replaces parent specs) or should change a table. The table will
+	-- be passed to the Plugin.config() function. Setting this value will imply
+	-- Plugin.config()
+	opts = {
+		position = "bottom", -- position of the list can be: bottom, top, left, right
+		height = 10, -- height of the trouble list when position is top or bottom
+		width = 50, -- width of the list when position is left or right
+		icons = true, -- use devicons for filenames
+		mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+		severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+		fold_open = "", -- icon used for open folds
+		fold_closed = "", -- icon used for closed folds
+		group = true, -- group results by file
+		padding = true, -- add an extra new line on top of the list
+		cycle_results = true, -- cycle item list when reaching beginning or end of list
+		action_keys = { -- key mappings for actions in the trouble list
+			-- map to {} to remove a mapping, for example:
+			-- close = {},
+			close = "q", -- close the list
+			cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+			refresh = "r", -- manually refresh
+			jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
+			open_split = { "<c-x>" }, -- open buffer in new split
+			open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+			open_tab = { "<c-t>" }, -- open buffer in new tab
+			jump_close = { "o" }, -- jump to the diagnostic and close the list
+			toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+			switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+			toggle_preview = "P", -- toggle auto_preview
+			hover = "K", -- opens a small popup with the full multiline message
+			preview = "p", -- preview the diagnostic location
+			open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
+			close_folds = { "zM", "zm" }, -- close all folds
+			open_folds = { "zR", "zr" }, -- open all folds
+			toggle_fold = { "zA", "za" }, -- toggle fold of current file
+			previous = "k", -- previous item
+			next = "j", -- next item
+			help = "?", -- help menu
+		},
+		multiline = true, -- render multi-line messages
+		indent_lines = true, -- add an indent guide below the fold icons
+		win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
+		auto_open = false, -- automatically open the list when you have diagnostics
+		auto_close = false, -- automatically close the list when you have no diagnostics
+		auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+		auto_fold = false, -- automatically fold a file trouble list at creation
+		auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
+		include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions" }, -- for the given modes, include the declaration of the current symbol in the results
+		signs = {
+			-- icons / text used for a diagnostic
+			error = "",
+			warning = "",
+			hint = "",
+			information = "",
+			other = "",
+		},
+		use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+	},
+
+	-- CONFIG
+	-- Config is executed when the plugin loads. The default implementation
+	-- will automatically run require(MAIN).setup(opts). Lazy uses several
+	-- heuristics to determine the plugin’s MAIN module automatically based
+	-- on the plugin’s name. See also opts. To use the default implementation
+	-- without opts set config to true.
+	--config = function ()
+	--    -- Put plugin settings into a local variable for easier reading
+	--    local PLUGIN_SETTINGS = {
+	--    }
+	--    require('plugin').setup(PLUGIN_SETTINGS)
+	--end,
+}
