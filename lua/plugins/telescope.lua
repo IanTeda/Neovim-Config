9cc6ecb6d5c9806e4ac39905af0db75178a4e064
Ian Teda <ian@teda.id.au>
2023-10-05 11:55:55 +1100

    Keep project telescope keymapping in the project plugin file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -173,8 +173,5 @@ return {
 		vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
 
-		-- vim.keymap.set("n", "<leader>sp", require("telescope.builtin").projects, { desc = "[S]earch [P]rojects" })
-
-
 	end,
 }
