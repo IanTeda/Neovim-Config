2286492f9a8a72e579fec56196370bd351265720
Ian Teda <ian@teda.id.au>
2026-07-27 08:36:07 +1000

    Create completion.lua

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "saghen/blink.cmp",
+    -- lazy = false required so lsp.lua can call get_lsp_capabilities() at BufReadPre
+    lazy = false,
+    version = "*",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<CR>"] = { "accept", "fallback" },
+      },
+      sources = {
+        default = { "lsp", "path", "buffer", "snippets" },
+      },
+      signature = {
+        enabled = true,
+        window  = { border = "rounded" },
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+          window    = { border = "rounded" },
+        },
+      },
+      fuzzy = {
+        use_frecency     = true,
+        use_proximity    = true,
+      },
+    },
+  },
+}
